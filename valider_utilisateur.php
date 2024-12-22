20a1ef51491fa8d0c71d9c2f40bd85c718867553
Nada Zirari <zirarinada20@gmail.com>
2024-12-22 20:11:53 +0100

    ajouter option de valider un user

---
+++ valider_utilisateur.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if ($_SESSION['role'] !== 'admin') {
+    header("Location: connexion.php");
+    exit;
+}
+
+require_once 'config.php';
+
+if (isset($_GET['id'])) {
+    $id_utilisateur = $_GET['id'];
+
+    // Mettre à jour l'utilisateur pour le valider
+    $stmt = $pdo->prepare("UPDATE utilisateurs SET valide = TRUE WHERE id = ?");
+    $stmt->execute([$id_utilisateur]);
+
+    // Redirection vers la page des nouvelles inscriptions après validation
+    header("Location: admin_dashboard.php");
+    exit;
+}
+?>
