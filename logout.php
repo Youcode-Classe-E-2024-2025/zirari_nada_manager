9402e23d494b7f5cb75366611cdb0497ed6f7b21
Nada Zirari <zirarinada20@gmail.com>
2024-12-22 20:07:58 +0100

    ajouter la section de deconnexion

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start(); // Démarre la session si elle n'est pas déjà démarrée
+
+// Détruire toutes les variables de session
+$_SESSION = [];
+
+// Détruire le cookie de session si existant
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, 
+        $params["path"], $params["domain"], 
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Détruire la session
+session_destroy();
+
+// Rediriger vers la page de connexion
+header("Location: connexion.php");
+exit;
+?>
