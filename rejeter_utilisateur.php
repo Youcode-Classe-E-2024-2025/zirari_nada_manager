fc36c7b460c282562ad7ca1e4304a7b0e2cec814
Nada Zirari <zirarinada20@gmail.com>
2024-12-22 20:10:33 +0100

    ajouter option de rejeter un user

---
+++ rejeter_utilisateur.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if ($_SESSION['role'] !== 'admin') {
+    header("Location: connexion.php");
+    exit;
+}
+
+require_once 'config.php';
+
+if (isset($_GET['id'])) {
+    $id_utilisateur = $_GET['id'];
+
+    // Supprimer l'utilisateur de la base de données ou l'archiver
+    $stmt = $pdo->prepare("UPDATE utilisateurs SET archive = TRUE WHERE id = ?");
+    $stmt->execute([$id_utilisateur]);
+
+    // Redirection vers la page des nouvelles inscriptions après rejet
+    header("Location: admin_dashboard.php");
+    exit;
+}
+?>
